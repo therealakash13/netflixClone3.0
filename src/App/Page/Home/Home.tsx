37c322a86b92018fff04ff73171c651e0e6c0631
Akash <therealakash13@gmail.com>
2025-07-07 07:15:21 +0000

    fetch movie and movie type and log in home

--- src/App/Page/Home/Home.tsx
+++ src/App/Page/Home/Home.tsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+import { fetchPopularMovies } from "../../Services/MovieFetch";
+import type { Movie } from "../../Services/movies.type";
+
 export default function HomePage() {
+  const [movie, setMovie] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setMovie(await fetchPopularMovies());
+    };
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    console.log(movie);
+  }, [movie]);
+
   return (
     <>
       <h1>Home</h1>
--- src/App/Services/MovieFetch.ts
+++ src/App/Services/MovieFetch.ts
@@ -1,16 +1,50 @@
+import axios from "axios";
+import type { Movie } from "./movies.type";
+
+// const options = {
+//   params: {
+//     include_adults: "false",
+//     includE_videos: "true",
+//     language: "en-US",
+//     sort_by: "popularity.desc",
+
+//     region: "IN",
+//   },
+//   method: "GET",
+//   headers: {
+//     accept: "application/json",
+//     Authorization:
+//       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTgzYWNlOTUzYTE3ZTE0MDEyYjJmMzk3ZTk5OTk0OSIsIm5iZiI6MTcxODI2OTY0OS40MzEsInN1YiI6IjY2NmFiNmQxNDgzN2NlYmFhZWE3NTAyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sgTgJ5BwuDCF75KgXJqiBapsAgrXdK8tGGnLUA-YhAI",
+//   },
+// };
+
 const options = {
   params: {
-    include_adults: "false",
-    includE_videos: "true",
+    include_adult: false,
+    include_video: false,
     language: "en-US",
+    page: 1,
     sort_by: "popularity.desc",
-
-    region: "IN",
   },
-  method: "GET",
   headers: {
     accept: "application/json",
     Authorization:
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwOTgzYWNlOTUzYTE3ZTE0MDEyYjJmMzk3ZTk5OTk0OSIsIm5iZiI6MTcxODI2OTY0OS40MzEsInN1YiI6IjY2NmFiNmQxNDgzN2NlYmFhZWE3NTAyZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sgTgJ5BwuDCF75KgXJqiBapsAgrXdK8tGGnLUA-YhAI",
   },
+};
+
+export const fetchPopularMovies = async (): Promise<Movie[]> => {
+  try {
+    console.log("Hello from MovieFetch");
+
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/discover/movie",
+      options
+    );
+
+    return response.data.results;
+  } catch (error) {
+    console.error("Error:", error);
+    return [];
+  }
 };
