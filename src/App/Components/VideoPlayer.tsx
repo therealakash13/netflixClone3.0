7aac79ad9da6bc7c35ae9ca4a41eeb5fb2e957a9
Akash <therealakash13@gmail.com>
2025-07-13 13:10:26 +0000

    reacttplayer integration

--- src/App/Components/VideoPlayer.tsx
+++ src/App/Components/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 interface VideoPlayerProps {
   videoUrl?: string;
@@ -6,11 +6,19 @@ interface VideoPlayerProps {
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
   console.log(videoUrl);
-  
+
   // if (!videoUrl) return null;
 
   return (
-    <ReactPlayer src={`https://www.youtube.com/watch?v=${videoUrl}`} />
+    <div className="w-[100vw] h-[100vh]">
+      <ReactPlayer
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+        src={`https://www.youtube.com/watch?v=${videoUrl}`}
+      />
+    </div>
   );
 };
 
