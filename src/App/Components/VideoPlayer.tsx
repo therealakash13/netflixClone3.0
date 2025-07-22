578348ca2c6c2d00b41ae5fd13c4738584db0bae
Akash <therealakash13@gmail.com>
2025-07-22 15:02:02 +0000

    detail page player setup

--- src/App/Components/VideoPlayer.tsx
+++ src/App/Components/VideoPlayer.tsx
@@ -10,13 +10,23 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoUrl }) => {
   // if (!videoUrl) return null;
 
   return (
-    <div className="w-[100vw] h-[100vh]">
+    // <div className="w-[100vw] h-[100vh]">
+    //   <ReactPlayer
+    //     style={{
+    //       width: "100%",
+    //       height: "100%",
+    //     }}
+    //     src={`https://www.youtube.com/watch?v=${videoUrl}`}
+    //   />
+    // </div>
+    <div className="w-[100vw] h-[calc(100vh-64px)] overflow-hidden">
       <ReactPlayer
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
         src={`https://www.youtube.com/watch?v=${videoUrl}`}
+        muted
+        playing
+        width="100%"
+        height="100%"
+        style={{ top: 0, left: 0 }}
       />
     </div>
   );
